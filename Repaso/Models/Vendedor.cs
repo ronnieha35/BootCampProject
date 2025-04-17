a299202f0a2d127cb18d642e985bbd6195080e7f
Ronnie Alarcon <ronnieha35@gmail.com>
2025-04-16 22:03:26 -0500

    RA 16-04-2025 : Generacion de Interfaz

---
+++ Repaso/Models/Vendedor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso.Models
+{
+    public class Vendedor : ITrabajador
+    {
+        public string Nombre { get; set; }
+        public decimal SueldoBase { get; set; }
+
+        public decimal CalcularComision(decimal sueldo)
+        {
+
+
+
+            return sueldo * 0.10M + sueldo;
+        }
+    }
+}
--- Repaso/Program.cs
+++ Repaso/Program.cs
@@ -6,11 +6,20 @@ namespace Repaso
     {
         static void Main(string[] args)
         {
-            Trabajador gerente = new Gerente("Ronnie Alarcon", 5000M);
-            Trabajador supervisor = new Supervisor("Miguel Loza", 4500M);
+            var gerente = new Gerente("Ronnie Alarcon", 5000M);
+            var supervisor = new Supervisor("Miguel Loza", 4500M);
+
+            var vendedor = new Vendedor();
+
+            
 
             Console.WriteLine($"{gerente.Nombre} bonificación: S/ {gerente.CalcularBonificacion()}");
             Console.WriteLine($"{supervisor.Nombre} bonificación: S/ {supervisor.CalcularBonificacion()}");
+            Console.WriteLine($"Comision {vendedor.CalcularComision(2500M)}");
+
+
+
+
         }
     }
 }
